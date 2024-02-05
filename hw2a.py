cd2701a938c435d7de3529b8b8fda8b35e16ad93
clintonpalmer <156226966+clintonpalmer@users.noreply.github.com>
2024-02-05 09:31:59 -0600

    added an output statement and removed the word "when".

--- hw2a.py
+++ hw2a.py
@@ -73,7 +73,7 @@ def main():
     c = 105
     print(f"P(x<{c}|N({mu},{sigma}))={Probability(Gauss_Norm_PDF, (mu, sigma), c, GT=False):.2f}")
 
-    # for Probability x is greater than c2
+    # for Probability x is greater than 2 standard deviations
     mu2, sigma2 = 100, 3
     c2 = mu2 + 2 * sigma2
     print(f"P(x>{mu2+2*sigma2}|N({mu2},{sigma2})={Probability(Gauss_Norm_PDF, (mu2,sigma2), c2, GT = True):.2f}")
